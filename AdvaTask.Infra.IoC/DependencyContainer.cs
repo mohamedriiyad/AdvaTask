fa94199c1b1e5f43b42a122a82d431d4d36c195b
Mohamed Riyad <mohamedriiyad@gmail.com>
2022-11-29 00:44:47 +0200

    Added DI Container and installed nuget package.

---
+++ AdvaTask.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AdvaTask.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Application Layer
+
+            //Infra.Data Layer
+        }
+    }
+}
