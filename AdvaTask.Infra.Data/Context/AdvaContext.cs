628f81afea50168fa70727eeee7a1376d8f02933
Mohamed Riyad <mohamedriiyad@gmail.com>
2022-11-28 23:39:48 +0200

    Added data model using EF Core.

---
+++ AdvaTask.Infra.Data/Context/AdvaContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AdvaTask.Infra.Data.Context
+{
+    public class AdvaContext : DbContext
+    {
+        public AdvaContext(DbContextOptions<AdvaContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
