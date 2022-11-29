b02eb16899725b8eae64c624307bdf93cd4aded9
Mohamed Riyad <mohamedriiyad@gmail.com>
2022-11-29 17:24:22 +0200

    Added full functionality to the Repository.

--- AdvaTask.Domain/Interfaces/IRepository.cs
+++ AdvaTask.Domain/Interfaces/IRepository.cs
@@ -1,13 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace AdvaTask.Domain.Interfaces
 {
     public interface IRepository <T> where T : class
     {
-        T Get(int id);
+        T GetById(int id);
         IEnumerable<T> GetAll();
-        void Add(T model);
-        void Delete(int id);
-        void Update(T model);
+        T Find(Expression<Func<T, bool>> criteria, string[] includes = null);
+        IEnumerable<T> FindAll(Expression<Func<T, bool>> criteria, string[] includes = null);
+        T Add(T entity);
+        IEnumerable<T> AddRange(IEnumerable<T> entities);
+        T Update(T entity);
+        void Delete(T entity);
+        void DeleteRange(IEnumerable<T> entities);
+        int Count();
+        int Count(Expression<Func<T, bool>> criteria);
     }
 }
--- AdvaTask.Infra.Data/Repository/Repository.cs
+++ AdvaTask.Infra.Data/Repository/Repository.cs
@@ -1,34 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using AdvaTask.Domain.Interfaces;
+using AdvaTask.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace AdvaTask.Infra.Data.Repository
 {
     public class Repository<T> :IRepository<T> where T : class
     {
-        public T Get(int id)
+        protected AdvaContext _context;
+
+        public Repository(AdvaContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
         public IEnumerable<T> GetAll()
         {
-            throw new NotImplementedException();
+            return _context.Set<T>().ToList();
         }
 
-        public void Add(T model)
+        public T GetById(int id)
         {
-            throw new NotImplementedException();
+            return _context.Set<T>().Find(id);
         }
 
-        public void Delete(int id)
+        public T Find(Expression<Func<T, bool>> criteria, string[] includes = null)
         {
-            throw new NotImplementedException();
+            IQueryable<T> query = _context.Set<T>();
+
+            if (includes != null)
+                foreach (var incluse in includes)
+                    query = query.Include(incluse);
+
+            return query.SingleOrDefault(criteria);
         }
 
-        public void Update(T model)
+        public IEnumerable<T> FindAll(Expression<Func<T, bool>> criteria, string[] includes = null)
         {
-            throw new NotImplementedException();
+            IQueryable<T> query = _context.Set<T>();
+
+            if (includes != null)
+                foreach (var include in includes)
+                    query = query.Include(include);
+
+            return query.Where(criteria).ToList();
+        }
+
+        public T Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            return entity;
+        }
+
+        public IEnumerable<T> AddRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().AddRange(entities);
+            return entities;
+        }
+
+        public T Update(T entity)
+        {
+            _context.Update(entity);
+            return entity;
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().RemoveRange(entities);
+        }
+
+        public int Count()
+        {
+            return _context.Set<T>().Count();
+        }
+
+        public int Count(Expression<Func<T, bool>> criteria)
+        {
+            return _context.Set<T>().Count(criteria);
         }
     }
 }
