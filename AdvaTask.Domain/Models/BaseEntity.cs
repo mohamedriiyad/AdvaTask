05ba6400620105ed576c9cce84eeef21bf14b450
Mohamed Riyad <mohamedriiyad@gmail.com>
2022-11-29 13:24:25 +0200

    Added BaseEntity

---
+++ AdvaTask.Domain/Models/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvaTask.Domain.Models
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
