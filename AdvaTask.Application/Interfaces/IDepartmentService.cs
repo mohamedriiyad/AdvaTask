a51552b3f416fdb871d64d1cb7616e904c327834
Mohamed Riyad <mohamedriiyad@gmail.com>
2022-11-30 18:52:45 +0200

    Added Department Service.

---
+++ AdvaTask.Application/Interfaces/IDepartmentService.cs
@@ -0,0 +1,16 @@
+﻿using AdvaTask.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvaTask.Application.Interfaces
+{
+    interface IDepartmentService
+    {
+        void AddDepartment(Department department);
+        Department GetDepartment(int id);
+        IEnumerable<Department> GetDepartments();
+        void UpdateDepartment(Department department);
+        void DeleteDepartment(Department department);
+    }
+}
---
+++ AdvaTask.Application/Services/DepartmentService.cs
@@ -0,0 +1,47 @@
+﻿using AdvaTask.Application.Interfaces;
+using AdvaTask.Domain.Interfaces;
+using AdvaTask.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvaTask.Application.Services
+{
+    public class DepartmentService : IDepartmentService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DepartmentService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void AddDepartment(Department department)
+        {
+            _unitOfWork.Departments.Add(department);
+            _unitOfWork.Complete();
+        }
+
+        public void DeleteDepartment(Department department)
+        {
+            _unitOfWork.Departments.Delete(department);
+            _unitOfWork.Complete();
+        }
+
+        public IEnumerable<Department> GetDepartments()
+        {
+            return _unitOfWork.Departments.GetAll(new[] { "Manager" });
+        }
+
+        public Department GetDepartment(int id)
+        {
+            return _unitOfWork.Departments.Find(d => d.Id == id, new[] { "Manager" });
+        }
+
+        public void UpdateDepartment(Department department)
+        {
+            _unitOfWork.Departments.Update(department);
+            _unitOfWork.Complete();
+        }
+    }
+}
