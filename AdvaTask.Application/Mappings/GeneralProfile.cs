4139e5326e2e48cd9c7c20ac3a15a4e9329d6356
Mohamed Riyad <mohamedriiyad@gmail.com>
2022-11-30 16:36:57 +0200

    Added new AutoMapper Profile.

---
+++ AdvaTask.Application/Mappings/GeneralProfile.cs
@@ -0,0 +1,17 @@
+﻿using AdvaTask.Application.DTOs;
+using AdvaTask.Domain.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvaTask.Application.Mappings
+{
+    public class GeneralProfile : Profile
+    {
+        public GeneralProfile()
+        {
+            CreateMap<AddEmployeeDTO, Employee>();
+        }
+    }
+}
