050b1061c18bdd28e586ce9686208cf860177d49
Mohamed Riyad <mohamedriiyad@gmail.com>
2022-12-01 11:39:15 +0200

    Modified Edit Employee component.

--- WebUI/ClientApp/src/app/edit-employee/edit-employee.component.ts
+++ WebUI/ClientApp/src/app/edit-employee/edit-employee.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Department } from '../_models/department';
 import { Employee } from '../_models/employee';
+import { DepartmentService } from '../_services/department.service';
 import { EmployeesService } from '../_services/employees.service';
 
 @Component({
@@ -11,23 +13,35 @@ import { EmployeesService } from '../_services/employees.service';
 export class EditEmployeeComponent implements OnInit {
   id: number;
   employee: Employee;
+  departments: Department[];
 
-  constructor(private route: ActivatedRoute, private employeeService: EmployeesService) { }
+  constructor(private route: ActivatedRoute,
+    private employeeService: EmployeesService,
+    private departmentSevice: DepartmentService) { }
 
   ngOnInit() {
-    this.route.params
-      .subscribe(params => {
-        this.id = params['id']; 
+    this.route.params.subscribe(params => this.id = params['id']);
+    this.getEmployee();
+    this.getDepartments();
+  }
+
+    private getDepartments() {
+        this.departmentSevice.getDepartments().subscribe(result => {
+            this.departments = result;
+        });
+    }
+
+    private getEmployee() {
+        this.employeeService.getEmployee(this.id).subscribe(result => {
+            this.employee = result;
+        });
+    }
+
+    save() {
+      this.employeeService.editEmployee(this.employee).subscribe(result => {
+        alert("Edited Successfully");
+      }, error => {
+          alert("Somthing went wrong");
       });
-
-    this.employeeService.getEmployee(this.id).subscribe(result => {
-      this.employee = result;
-      console.log(this.employee);
-    })
   }
-  log() {
-    console.log(this.employee.name)
-    console.log(this.employee.salary)
-  }
-
 }
