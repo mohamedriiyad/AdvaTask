610768beff73f710907b7b36a58911bdc872b110
Mohamed Riyad <mohamedriiyad@gmail.com>
2022-12-05 00:54:28 +0200

    Added Global Exception Handling.

--- WebUI/Startup.cs
+++ WebUI/Startup.cs
@@ -1,12 +1,15 @@
 using AdvaTask.Infra.Data.Context;
 using AdvaTask.Infra.IoC;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Net;
 
 namespace WebUI
 {
@@ -51,6 +54,18 @@ namespace WebUI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
+            app.UseExceptionHandler(config =>
+            {
+                config.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    var error = context.Features.Get<IExceptionHandlerFeature>();
+                    
+                    if (error != null)
+                        await context.Response.WriteAsync(error.Error.Message);
+                });
+            });
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
